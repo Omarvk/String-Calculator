9e6deda53b462769c8b16349614d3f6d3b0210fe
Omarv <omarkval@gmail.com>
2015-10-11 17:16:20 +0000

    2. Allow the Add method to handle an unknown number of
    arguments/numbers.  skips if string is not number

--- Calculatort.java
+++ Calculatort.java
@@ -9,15 +9,30 @@ Public class Calcus{
 		return fixstring(numbers);
 		
 	};
+	
 	private int fixstring(String input){
 		input = input.replace("\\n" , ",");
 		String[] numbers = input.split(",");
 		int i = 0;
 		
 		while(i != numbers.length){
-			sum += Integer.parseInt(numbers[i]);
+			isNumber(number[i].trim()){
+				sum += Integer.parseInt(numbers[i].trim());
+			}
 			i++;
 		}
 		return sum;
+	}
+	
+	private  Boolean isNumber(String numbers){
+		try
+		{
+			int number = Integer.parseInt(numbers);
+		}
+		catch(NumberFormatException villa)
+		{
+			return false;
+		}
+		return true;
 	}
 }
